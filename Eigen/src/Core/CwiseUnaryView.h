9126c06a419fd62b3f4eee70581b44f3a492b7ff
Ger Hobbelt <ger@hobbelt.com>
2024-03-12 15:59:46 +0100

    std::result_of is deprecated (C++17) or removed (C++20)

--- Eigen/src/Core/CwiseUnaryView.h
+++ Eigen/src/Core/CwiseUnaryView.h
@@ -18,7 +18,8 @@ namespace Eigen {
 namespace internal {
 template <typename ViewOp, typename MatrixType, typename StrideType>
 struct traits<CwiseUnaryView<ViewOp, MatrixType, StrideType> > : traits<MatrixType> {
-  typedef typename std::result_of<ViewOp(typename traits<MatrixType>::Scalar&)>::type ScalarRef;
+  // std::result_of is deprecated (C++17) or removed (C++20)
+  typedef typename std::invoke_result<ViewOp(typename traits<MatrixType>::Scalar&)>::type ScalarRef;
   static_assert(std::is_reference<ScalarRef>::value, "Views must return a reference type.");
   typedef remove_all_t<ScalarRef> Scalar;
   typedef typename MatrixType::Nested MatrixTypeNested;
